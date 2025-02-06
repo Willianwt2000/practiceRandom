6dac5efa6d0db17e32b58f07c71aa7392c16c5ae
willianwt2000 <willianmetelien@gmail.com>
2025-02-05 21:44:53 -0400

    Area triangulo fucionado con cherypick

--- areaCircle.js
+++ areaCircle.js
@@ -4,3 +4,9 @@ function gradoCelsius(c) {
     console.log("\n\n    ");
 }
 console.log(gradoCelsius(25));
+//Area de un trianguglo
+function areaTriangle(base, altura) {
+    var area = (base * altura) / 2;
+    return area;
+}
+console.log(areaTriangle(5, 3));
--- areaCircle.ts
+++ areaCircle.ts
@@ -11,3 +11,13 @@ function gradoCelsius(c:number) {
 console.log(gradoCelsius(25));
 
 
+
+//Area de un trianguglo
+
+function areaTriangle(base:number, altura:number): number {
+  let area = (base * altura) / 2
+  return area;
+}
+
+console.log(areaTriangle(5,3))
+
