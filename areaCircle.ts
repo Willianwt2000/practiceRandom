5fb64e87e0e2ca5da3c54c1949f61a2779a7f9a0
willianwt2000 <willianmetelien@gmail.com>
2025-02-05 21:44:53 -0400

    Area de un triangulo

---
+++ areaCircle.ts
@@ -0,0 +1,22 @@
+
+// F=(°C× 5/9)+32  -- conversion de grado celcious a farenheig
+
+function gradoCelsius(c:number) {
+  const F = (c * (9/5) + 32)
+  console.log(`
+
+    `)
+}
+
+console.log(gradoCelsius(25));
+
+
+//Area de un trianguglo
+
+function areaTriangle(base:number, altura:number): number {
+  let area = (base * altura) / 2
+  return area;
+}
+
+console.log(areaTriangle(5,3))
+
