910aed9b7d63ee4372033a15a920686f73560599
willianwt2000 <willianmetelien@gmail.com>
2025-02-05 21:11:39 -0400

    cambios

--- sumarPare.js
+++ sumarPare.js
@@ -1,12 +1,11 @@
 //? Sumar pares
 //Input: [1, 2, 3, 4, 5, 6] 
 //output: 12
-var nums = [1, 2, 3, 4, 5, 6];
+var nums = [10, 21, 32, 47, 56];
 var acumulated = 0;
 for (var i = 0; i < nums.length; i++) {
     if (nums[i] % 2 === 0) {
         acumulated += nums[i];
-        console.log(acumulated);
     }
 }
 console.log(acumulated);
--- sumarPare.ts
+++ sumarPare.ts
@@ -5,7 +5,7 @@
 
 
 
-const nums:number[] = [1, 2, 3, 4, 5, 6] 
+const nums:number[] = [10, 21, 32, 47, 56]
 let acumulated :number= 0
 for (let i = 0; i < nums.length; i++) {
   if(nums[i] % 2 === 0) {
